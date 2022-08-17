be42c9e2b20bbe78bc7e57c10dfc577f6587d005
UwUssimo Robinson <uwussimo@icloud.com>
2022-08-17 17:17:44 +0500

    allocate max 5 gigs to server

--- src/launch.rs
+++ src/launch.rs
@@ -3,7 +3,7 @@ use crate::execute;
 pub async fn start() {
     execute::run(
         "java",
-        vec!["-Xmx1024M", "-Xms1024M", "-jar", "server.jar", "nogui"],
+        vec!["-Xmx5120M", "-Xms1024M", "-jar", "server.jar", "nogui"],
     )
     .await
 }
